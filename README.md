2da889af7931285701eb04221ccca25c4572e5de
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-29 15:29:25 +0200

    [UPD] Index route created and it works

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Creiamo una nuova applicazione Express
 Colleghiamo l’app al db e verifichiamo che tutto funzioni
 Prepariamo una rotta index per ottenere la lista dei film
 Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
+
 Numero minimo di push: da oggi non vi comunicherò il numero minimo di push, dovendo lavorare su più giorni sulla stessa repo
 
 Bonus
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,7 +4,14 @@ const connection = require('../data/db')
 const index = (req, res) => {
     const mysql = "SELECT * FROM movies";
     // eseguro la query
+    connection.query(mysql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: "Database query failed" })
+        }
+        res.json(results)
+    })
 }
+
 const show = (req, res) => {
     const id = req.params.id
 
